bb80396fa831b2d8644c1522e2d4673fb079ea9a
David Dahlgren <ddaviddahlgren@gmail.com>
2026-09-11 15:37:37 +0200

    removed /authos link

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -49,14 +49,13 @@ function NavItem({ item }) {
       onMouseEnter={() => setOpen(true)}
       onMouseLeave={() => setOpen(false)}
     >
-      <Link
-        href={resolveHref(item.link)}
-        className="relative font-bold uppercase text-white after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:w-full after:origin-left after:scale-x-0 after:bg-white after:transition-transform after:duration-300 after:ease-out hover:after:scale-x-100"
-        aria-expanded={hasChildren ? open : undefined}
-        aria-haspopup={hasChildren ? "true" : undefined}
-      >
+      <span
+          className="relative cursor-default font-bold uppercase text-white"
+          aria-expanded={open}
+          aria-haspopup="true"
+        >
         {item.label}
-      </Link>
+      </span>
 
       {hasChildren && (
         <ul
